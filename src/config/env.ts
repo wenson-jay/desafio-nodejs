634066b92416eec9f461578a3e247c9cc34a782f
alnmaurofranco <jefeson23@live.com>
2025-08-14 14:48:51 -0300

    feat: add configuration safe env

---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  DATABASE_URL: z.url().startsWith('postgresql://'),
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().default(5555),
+});
+
+if (!envSchema.safeParse(process.env).success) {
+  const error = envSchema.safeParse(process.env).error;
+  throw new Error(`Invalid environment variables: ${error}`);
+}
+
+export const env = envSchema.parse(process.env);
